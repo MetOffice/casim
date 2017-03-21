69c549096c2a9c504ae0ce5f633d25dbcd848497
jonathanwilkinson <enquiries@metoffice.gov.uk>
2017-03-21 13:32:43 +0000

    ticket:141: For Jonathan Wilkinson: Further commit to ensure that MONC still builds with the dummy cloud fraction.
    git-svn-id: https://metomi/svn/monc.xm/casim/trunk@2790 0f676ef4-b20c-4647-9485-21614760d15f

--- src/cloud_fraction_dummy.F90
+++ src/cloud_fraction_dummy.F90
@@ -9,11 +9,14 @@ CONTAINS
 SUBROUTINE cloud_frac_casim_mphys(k, pressure, abs_liquid_t, rhcrit_lev, &
                                   qs, qv, cloud_mass, qfields, cloud_mass_new)
 
+USE variable_precision, ONLY: wp
+
 IMPLICIT NONE
 INTEGER,  INTENT(IN)  :: k
-REAL(wp), INTENT(IN)  :: pressure, abs_liquid_t, rhcrit_lev, qs, qv cloud_mass, qfields
-INTEGER,  INTENT(IN)  :: k
+REAL(wp), INTENT(IN)  :: pressure, abs_liquid_t, rhcrit_lev, qs, qv, cloud_mass, qfields
 REAL(wp), INTENT(OUT) :: cloud_mass_new
+
+cloud_mass_new = cloud_mass
 
 END SUBROUTINE cloud_frac_casim_mphys
 
