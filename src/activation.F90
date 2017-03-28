60bff832011206d11e7fb938db26f270afcd0c9a
jonathanwilkinson <enquiries@metoffice.gov.uk>
2017-03-28 13:56:11 +0000

    ticket:165: For Ben Shipway: Bugfix to prevent a loss of number with processing - will change answers in processing jobs.
    git-svn-id: https://metomi/svn/monc.xm/casim/trunk@2866 0f676ef4-b20c-4647-9485-21614760d15f

--- src/activation.F90
+++ src/activation.F90
@@ -2,7 +2,7 @@ module activation
   use variable_precision, only: wp
   use mphys_constants, only: fixed_cloud_number
   use mphys_parameters, only: C1, K1
-  use mphys_switches, only: iopt_act, iopt_rcrit, aero_index,   &
+  use mphys_switches, only: iopt_act, aero_index,   &
        i_am2, i_an2, i_am4, i_am1, i_an1, i_am3, i_an3, i_am5, i_am6, i_an6, i_am7, l_warm
   use aerosol_routines, only: aerosol_active, aerosol_phys, aerosol_chem, &
        abdulRazzakGhan2000, invert_partial_moment, upperpartial_moment_logn, &
@@ -10,12 +10,6 @@ module activation
   use special, only: erfinv, erf, pi
   use thresholds, only: w_small, nl_tidy, ccn_tidy
 
-#if DEF_MODEL==MODEL_KiD
-  use diagnostics, only: save_dg, i_dgtime, k_here, i_here
-  use runtime, only: time
-#elif  DEF_MODEL==MODEL_MONC
-  use diaghelp_monc, only: i_here, j_here
-#endif
   implicit none
   private
 
@@ -37,7 +31,7 @@ contains
     type(aerosol_active), intent(in) :: dustliq
     real(wp), intent(out) :: dnccn_all(:),dmac_all(:)
     real(wp), intent(out) :: dnumber_d, dmass_d ! activated dust number and mass
-    real(wp) :: active, nccn, Smax, ucrit, rcrit_ARG, rcrit, nccn_active
+    real(wp) :: active, nccn, Smax, rcrit, nccn_active
     real(wp) :: Nd, rm, sigma, density
 
     real :: work, dmac_i, diff
@@ -48,6 +42,10 @@ contains
     dmac=0.0
     dnumber_d=0.0
     dmass_d=0.0
+    dnumber=0.0
+    dmac=0.0
+    dmac_all=0.0
+    dnccn_all=0.0
 
     select case(iopt_act)
     case default
@@ -63,77 +61,58 @@ contains
     case(3)
       ! Use scheme of Abdul-Razzak and Ghan
       if (w > w_small .and. sum(aerophys%N(:)) > ccn_tidy) then
-        !        call AbdulRazzakGhan(w, cloud_mass, p, T, aerophys, aerochem, nccn, Smax, rcrit_ARG)
         call AbdulRazzakGhan2000(w, p, T, aerophys, aerochem, dnccn_all, Smax, aeroact, &
              nccn_active, l_useactive)
         active=sum(dnccn_all(:))
 
         if (Smax > 0.02 .and. .not. l_warm) then
           ! need better model than this. Could do partitioning in the same way as CCN
-          dnumber_d=.01*dustphys%N(1)/dt
+          dnumber_d=.01*dustphys%N(1)
           dmass_d=dnumber_d*dustphys%M(1)/dustphys%N(1)
         end if
       else
         active=0.0
       end if
-
-#if DEF_MODEL==MODEL_KiD
-      call save_dg(k_here, Smax, 'Smax', i_dgtime)
-      call save_dg(k_here, dnumber_d, 'dnumber_d', i_dgtime)
-      call save_dg(k_here, dmass_d, 'dmass_d', i_dgtime)
-      call save_dg(k_here, dustphys%N(1), 'dustphys%N(1)', i_dgtime)
-      call save_dg(k_here, dustphys%M(1), 'dustphys%M(1)', i_dgtime)
-      call save_dg(k_here, i_here, sum(aerophys%N(:)), 'sumN_ARG', i_dgtime)
-      call save_dg(k_here, i_here, rcrit_ARG, 'rcrit_ARG', i_dgtime)
-      call save_dg(k_here, i_here, active, 'nccn_ARG', i_dgtime)
-#endif
     end select
 
-    if (active < nl_tidy) then
-      dnumber=0.0
-      dmac=0.0
-      dmac_all=0.0
-      dnccn_all=0.0
-
-    else
-#if DEF_MODEL==MODEL_KiD
-      if (cloud_Number > 0 .and. iopt_act==3) then
-        call save_dg(k_here, i_here, (cloud_number - nccn_active), 'nccn_active_extra', i_dgtime)
-        call save_dg(k_here, i_here, (nccn_active/cloud_number), 'nccn_active_fraction', i_dgtime)
-      end if
-#endif
-
-      ! Need to make this consistent with all aerosol_options
-      do imode = 1, aero_index%nccn
-        Nd=aerophys%N(imode)
-        if (Nd > ccn_tidy) then
-          rm=aerophys%rd(imode)
-          sigma=aerophys%sigma(imode)
-          density=aerochem%density(imode)
-
-          select case (iopt_rcrit)
-          case default
-            rcrit=invert_partial_moment_betterapprox(dnccn_all(imode), 0.0_wp, Nd, rm, sigma)
-          case(2)
-            rcrit=invert_partial_moment(Nd, dnccn_all(imode), 0.0_wp, rm, sigma)
-          case(3)
-            rcrit=invert_partial_moment_approx(dnccn_all(imode)/Nd, 0.0_wp, rm, sigma)
-          end select
-
-          dmac_all(imode)=(4.0*pi*density/3.0)*(upperpartial_moment_logn(Nd, rm, sigma, 3.0_wp, rcrit))
-          dmac_all(imode)=min(dmac_all(imode),0.999*aerophys%M(imode)) ! Don't remove more than 99.9%
-          dmac=dmac+dmac_all(imode)
+    select case(iopt_act)
+    case default
+      ! fixed number, so no need to calculate aerosol changes
+      dnumber=max(0.0_wp,(active-cloud_number))
+    case (1:4)
+      if (active > nl_tidy) then
+        if (l_useactive) then
+          dnumber=active
+        else
+          dnumber=max(0.0_wp,(active-cloud_number))
+          ! Rescale to ensure total removal of aerosol number=creation of cloud number
+          dnccn_all = dnccn_all*(dnumber/(sum(dnccn_all) + tiny(dnumber)))
         end if
-      end do
-      ! Need to make this consistent with all aerosol_options
-      dmac=dmac/dt
-      dmac_all=dmac_all/dt
-      dnccn_all=dnccn_all/dt
-      if (l_useactive) then
-        dnumber=active/dt
-      else
-        dnumber=max(0.0_wp,(active-cloud_number)/dt)
+        ! Need to make this consistent with all aerosol_options
+        do imode = 1, aero_index%nccn
+          Nd=aerophys%N(imode)
+          if (Nd > ccn_tidy) then
+            rm=aerophys%rd(imode)
+            sigma=aerophys%sigma(imode)
+            density=aerochem%density(imode)
+            
+            rcrit=invert_partial_moment_betterapprox(dnccn_all(imode), 0.0_wp, Nd, rm, sigma)
+            
+            dmac_all(imode)=(4.0*pi*density/3.0)*(upperpartial_moment_logn(Nd, rm, sigma, 3.0_wp, rcrit))
+            dmac_all(imode)=min(dmac_all(imode),0.999*aerophys%M(imode)) ! Don't remove more than 99.9%
+            dmac=dmac+dmac_all(imode)
+          end if
+        end do
       end if
-    end if
+    end select
+
+    ! Convert to rates rather than increments
+    dmac=dmac/dt
+    dmac_all=dmac_all/dt
+    dnccn_all=dnccn_all/dt
+    dnumber=dnumber/dt
+    dnumber_d=dnumber_d/dt
+    dmass_d=dmass_d/dt
+
   end subroutine activate
 end module activation
